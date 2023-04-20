1fe348a5e76334a3ecfb0c4339b3069630affe37
Shujat Ali <Shujat.dev@gmail.com>
2023-04-20 10:19:09 -0500

    added fragment fav and room db

---
+++ app/src/main/java/com/miu/movieapp/db/MovieItem.kt
@@ -0,0 +1,15 @@
+package com.miu.movieapp.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity
+data class MovieItem(//  @ColumnInfo(name = "notetitle")
+    val title :String,
+    val note :String
+): Serializable // need to pass Note entity through Fragments. So that entity should be Serializable
+{
+    @PrimaryKey(autoGenerate = true)
+    var id:Int = 0
+}
--- app/src/main/java/com/miu/movieapp/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/miu/movieapp/fragment/FavoriteFragment.kt
@@ -2,12 +2,14 @@ package com.miu.movieapp.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.miu.movieapp.R
 import com.miu.movieapp.databinding.FragmentFavoriteBinding
-import com.miu.movieapp.databinding.FragmentMovieBinding
+import com.miu.movieapp.db.MovieDatabase
+import com.miu.movieapp.db.MovieItem
+import com.miu.movieapp.other.toastShort
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,10 +23,61 @@ private const val ARG_PARAM2 = "param2"
  */
 class FavoriteFragment : BaseFragment() {
     lateinit var binding : FragmentFavoriteBinding
+    var array = arrayOf("").toList()
 
     override fun onCreateView(): View {
         binding = FragmentFavoriteBinding.bind(rootView)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        reloadData()
+
+        binding.btnAdd.setOnClickListener {
+            val noteTitle = "Note : " + array.size
+            val noteBody = binding.edtFavs.text.toString()
+            // Check the input values are empty, then set the error message and give the focus
+
+            if(noteBody.isEmpty()){
+                binding.edtFavs.error = "Body Required"
+                binding.edtFavs.requestFocus()
+                return@setOnClickListener // stop further execution ie returning at the end of the setOnClickListener
+            }
+
+            launch {
+                /* NoteDatabase needs an context argument, if it is not null
+                let perform add or update query
+                */
+                context?.let {
+                    // Create a new Note object
+                    val mNote = MovieItem(noteTitle, noteBody)
+
+                    MovieDatabase(it).getNoteDao().addNote(mNote)
+                    it.toastShort("Note Saved")
+                    reloadData()
+                }
+            }
+        }
+    }
+
+    fun reloadData() {
+        launch {
+            /* here context is the getContext() from the Fragment, if the context
+            * is not null, let's execute the block of code using let scope function
+            * with the argument it's context object - Inline functions
+            * similar like  if(context!=null){}*/
+            context?.let{
+                val notes = MovieDatabase(it).getNoteDao().getAllNotes()
+                array = notes.map { it.title + it.note }
+
+                var adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, array)
+                adapter.setNotifyOnChange(true)
+                binding.listviewFavs.adapter = adapter
+            }
+        }
     }
 
     override fun getLayout(): Int {
